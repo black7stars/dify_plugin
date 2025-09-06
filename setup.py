f7162799c3fbf258f63f1331ff26f085735c1e7a
black7stars <black7stars@hotmail.com>
2025-09-06 16:57:31 +0800

    初始项目配置

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="dify_plugin",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        # 你的依赖项
+    ],
+    extras_require={
+        "dev": [
+            "black",
+            "flake8",
+            "pytest",
+            "pre-commit",
+        ],
+    },
+)
